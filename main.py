a01c9b2b917e185f71b5dad1cf651736dfb2e2dd
Damián Brizuela <dbrizuela@zoonsuite.com>
2025-05-09 15:27:59 -0300

    telegram bot functional

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackContext, MessageHandler, filters
 
-import os
 from dotenv import load_dotenv
+import os
+
 load_dotenv()
+
+Token = os.getenv("TOKEN_TELEGRAM")
 
 def trace(message):
     print(f'::: [CHATBOT] {message}')
 
-Token = os.getenv("TOKEN_TELEGRAM")
-
 async def start(update: Update, context: CallbackContext):
-    await update.message.reply_text('Como te encuentas wey!!')
+    await update.message.reply_text('Como te encuentas Usuario?')
+
+async def bye(update: Update, context: CallbackContext):
+    await update.message.reply_text('Adios, que estes bien!!')
+
+async def chat(update: Update, context: CallbackContext):
+    print(update.message.text)
+    await update.message.reply_text("Chacharas")
 
 def main():
+    
     trace('RUNNING!')
-    trace(Token)
     app = Application.builder().token(Token).build()
     app.add_handler(CommandHandler('start', start))
+    app.add_handler(CommandHandler('bye', bye))
+    app.add_handler(MessageHandler(filters.TEXT, chat))
     app.run_polling()
 
 if __name__ == '__main__':
